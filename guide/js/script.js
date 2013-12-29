c178fef8e3aa4fec0a423306f7cdb88d37cca3bc
Sten Hougaard <netsi1964@gmail.com>
2013-12-29 08:44:29 +0100

    Rework on design
    * Moved the end time inside time range element
    * Deleted end time column
    * Removed tv-channel logo

--- guide/js/script.js
+++ guide/js/script.js
@@ -1,25 +1,66 @@
+var $globalscope;
 function FetchCtrl($scope, $http, $templateCache) {
+    $globalscope = $scope;
     $scope.method = 'GET';
     $scope.url = 'http://yousee.tv/rest/tvguide/nowandnext/API-Key/cMy34N4qFOUfq5WfesmvgiEdcC6PCyHa6JYqOMbq';
+    $scope.epg = 'http://guide.netsi.dk/get.php?url=http://yousee.tv/feeds/tvguide/dailyjson/8/&mimeType=JSON&headers=';
+    $scope.categories = [];
+    $scope.catSort = ['c','s'];
+
     $scope.fetch = function() {
         $scope.code = null;
         $scope.response = null;
+        $scope.excludeChannels = {'14':false, '11':false}
 
+        // EPG
         $http({
-            method: $scope.method,
-            url: $scope.url,
-            cache: $templateCache
+            method: 'get',
+            url: $scope.epg+'?random='+(new Date()-new Date(2014,1,1))
         }).
         success(function(data, status) {
-            $scope.status = status;
-            $scope.data = data;
-            console.log(data)
+            $scope.epg_status = status;
+            $scope.epg_data = data;
+
+            // Channels
+            $http({
+                method: $scope.method,
+                url: $scope.url+'?random='+(new Date()-new Date(2014,1,1)),
+                cache: $templateCache
+            }).
+            success(function(data, status) {
+                data.now = $scope.customChannels(data.now);
+                for(var i=0; i<data.now.length; i++) {
+                    data.now[i].epg =  $scope.epg_data[data.now[i].channel];
+                    var sCat = data.now[i].category_string+' - '+data.now[i].subcategory_string;
+                    if (typeof $scope.categories[sCat]==='undefined') {
+                        $scope.categories[sCat] = 1;
+                        $scope.categories.push({c:data.now[i].category_string, s:data.now[i].subcategory_string});
+                    }
+                }
+                $scope.status = status;
+                $scope.data = data;
+            }).
+            error(function(data, status) {
+                $scope.data = data || "Request failed";
+                $scope.status = status;
+            });
         }).
         error(function(data, status) {
-            $scope.data = data || "Request failed";
-            $scope.status = status;
+            $scope.epg_data = data || "Request failed";
+            $scope.epg_status = status;
         });
+
+
+
     };
+
+    $scope.customChannels = function(dataNow) {
+        var newData = [];
+        for(var i=0; i<dataNow.length; i++) {
+            if ($scope.excludeChannels[dataNow[i].channel]!==false) newData.push(dataNow[i]);
+        }
+        return newData;
+    }
 
     $scope.progress = function(sStart, sEnd) {
         var from = parseInt(sStart.replace(/\./g, ''));
@@ -47,6 +88,17 @@ function FetchCtrl($scope, $http, $templateCache) {
         window.open('http://yousee.tv/livetv/' + schannel + '/');
     }
 
+    $scope.isCat = function() {
+        var bHit = true;
+        if (bHit) {
+            bHit =  (typeof $scope.cat==='undefined' || $scope.cat=='') ? true : arguments[0].category_string+' - '+arguments[0].subcategory_string===$scope.cat;
+            if (bHit) {
+                bHit = (typeof $scope.search==='undefined') ? true : (JSON.stringify(arguments[0]).toLowerCase().indexOf($scope.search.toLowerCase())>-1)
+            }
+        };
+        return bHit;
+    }
+
     $scope.updateModel = function(method, url) {
         $scope.method = method;
         $scope.url = url;
@@ -55,10 +107,13 @@ function FetchCtrl($scope, $http, $templateCache) {
     $scope.fetch();
     window.setInterval(function() {
         $scope.fetch();
+
     }, 60 * 1000);
 
+    $scope.reverse = false;
     $scope.predicate = 'channel_info.name';
     $scope.filter = function(desc) {
         return desc;
     }
 }
+
